b4500a9eecdb524f89242b2dd40dc85fd7c8ae00
soldev21 <sherif.huseynli@gmail.com>
2021-03-06 02:36:51 +0400

    last

--- src/main/java/com/rest/soap/wrapper/configuration/RestTemplateConfig.java
+++ src/main/java/com/rest/soap/wrapper/configuration/RestTemplateConfig.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import sun.net.www.http.HttpClient;
 
-/**
- * Created by Sherif on 8/27/2017.
- */
 @Component
 public class RestTemplateConfig {
 
